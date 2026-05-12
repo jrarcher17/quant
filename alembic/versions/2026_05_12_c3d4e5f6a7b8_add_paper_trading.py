995dbc4c70395d3fb1332c259e9ab5b8ae8ff487
John Archer <jrarcher17@gmail.com>
2026-05-12 13:54:37 -0700

    paper trading tables fix

--- alembic/versions/2026_05_12_c3d4e5f6a7b8_add_paper_trading.py
+++ alembic/versions/2026_05_12_c3d4e5f6a7b8_add_paper_trading.py
@@ -9,7 +9,7 @@ from alembic import op
 import sqlalchemy as sa
 
 revision = "c3d4e5f6a7b8"
-down_revision = "b2c3d4e5f6a7"
+down_revision = "5bdb871466f7"
 branch_labels = None
 depends_on = None
 
